0c6ee0117c33ed8363d405797e9932a9fdb8dfc0
Dicky Agus Setiawan <dickyagussetiawan@gmail.com>
2022-06-21 15:14:05 +0700

    [FEATURE] 03.variable

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,22 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  let nama = "Dicky Agus Setiawan";
+  let skill = {
+    namaSkill : ["Svelte", "PHP", "ReactJs", "VueJs"],
+    ratingSkill : 90
+  };
+  let imageHome = "img/otw-lambo.jpg"
+  
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <h3>Welcome, <span class="nama">{nama}</span></h3>
+  <p>Skill : <b>{skill.namaSkill[0]} dan {skill.namaSkill[3]}</b></p>
+  <p>Rating : {skill.ratingSkill}</p>
+  <img src="{imageHome}" alt="Home" width="300">
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  .nama {
+    color: red;
   }
 </style>
